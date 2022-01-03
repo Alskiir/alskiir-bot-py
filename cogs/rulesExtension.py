bdadee0f93a9f2619cf43363b5c84c962b4016c1
Alskiir <Doughart215@cfl.rr.com>
2022-01-02 19:03:19 -0500

    color

--- cogs/rulesExtension.py
+++ cogs/rulesExtension.py
@@ -3,31 +3,34 @@ from discord.ext.commands import view
 from discord.ui import Button, View
 from discord.ext import commands
 
-#change the server ids
-server_list =[875236232303636491] # alskiir's server id
+# change the server ids
+server_list = [875236232303636491]  # alskiir's server id
+
 
 class myButtons(Button):
     def __init__(self, style, label):
         super().__init__(style=style, label=label)
-    
+
     async def callback(self, interaction):
-        #gets the user who clicked the button
+        # gets the user who clicked the button
         user = interaction.user
-        #role id
+        # role id
         print(f'user roles {user.roles}')
-        role = interaction.guild.get_role(875242889943347211) #lettuce role id
+        role = interaction.guild.get_role(
+            875242889943347211)  # lettuce role id
 
         if role not in user.roles:
             await user.add_roles(role)
             await interaction.response.send_message(f'given role {role.mention}', ephemeral=True)
         else:
             await user.remove_roles(role)
-            await interaction.response.send_message(f'taken role {role.mention}',ephemeral=True)
-        
+            await interaction.response.send_message(f'taken role {role.mention}', ephemeral=True)
+
+
 class Alskiir(commands.Cog):
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
-    
+
     # @commands.user_command(name='alskiir',guild_ids=server_list)
     @commands.command(name='alskiir')
     async def alskiir(self, ctx):
@@ -36,21 +39,27 @@ class Alskiir(commands.Cog):
         # **Get Your Role!**
 
         # the button below to get your role and see the rest of the server!
-    
+
         # **For Twitch Subscribers!**
-        
+
         # Link your Twitch account to your Discord account for emotes and roles!
         # '''
-        desc='-----------------------'
-        embed = discord.Embed(title=title,description=desc)
-        embed.set_image(url='https://cdn.discordapp.com/attachments/484432102402555935/926974132539781140/no-background-shadow-cropped.png')
-        embed.add_field(name='Get Your Role!', value= 'Select the button below to get your role and see the rest of the server!')
-        embed.add_field(name='For Twitch Subscribers!', value='Link your Twitch account to your Discord account for emotes and roles!' )
-        alskiir_button = myButtons(style=discord.ButtonStyle.primary,label='Join the Fun!')
+        desc = '-----------------------'
+        embed = discord.Embed(title=title, description=desc)
+        embed.set_image(
+            url='https://cdn.discordapp.com/attachments/484432102402555935/926974132539781140/no-background-shadow-cropped.png')
+        embed.add_field(name='Get Your Role!',
+                        value='Select the button below to get your role and see the rest of the server!')
+        embed.add_field(name='For Twitch Subscribers!',
+                        value='Link your Twitch account to your Discord account for emotes and roles!')
+        embed.color = '#0099ff'
+        alskiir_button = myButtons(
+            style=discord.ButtonStyle.primary, label='Join the Fun!')
         view = View()
         view.add_item(alskiir_button)
-        await ctx.send(embed=embed,view=view)
+        await ctx.send(embed=embed, view=view)
+
 
 def setup(bot):
-    bot.add_cog(Alskiir(bot) )
+    bot.add_cog(Alskiir(bot))
     print('loading Alskiir')
