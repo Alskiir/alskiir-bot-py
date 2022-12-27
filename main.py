1f512c876e31a8e9a6b5613e34524543530a57fa
Alskiir <Doughart215@cfl.rr.com>
2022-12-27 15:38:38 -0500

    main.py async, -Procfile, py runtime
    Added async func to main.py for discord.py 2.0 async changes.
    
    Updated runtime of python from 3.10.1 to 3.11.1
    
    Removed Procfile because it was only used when the bot was hosted on Heroku

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-import discord, os
-token= os.environ["TOKEN"]
 from discord.ext import commands
+import discord
+import os
+import asyncio
+token = os.environ["TOKEN"]
 
 intents = discord.Intents.all()
 intents.members = True
-bot = commands.Bot(command_prefix='-',intents=intents)
+bot = commands.Bot(command_prefix='-', intents=intents)
+
 
 @bot.event
 async def on_ready():
     print(f'logged in as {bot.user}')
 
 
-bot.load_extension('cogs.rulesExtension')
-bot.run(token)
+async def main():
+    async with bot:
+        bot.load_extension('cogs.rulesExtension')
+        await bot.start(token)
+
+asyncio.run(main())
+
+# bot.load_extension('cogs.rulesExtension')
+# bot.run(token)
